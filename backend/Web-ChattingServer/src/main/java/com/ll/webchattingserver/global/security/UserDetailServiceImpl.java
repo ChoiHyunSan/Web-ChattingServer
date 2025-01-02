bfef07992fc48b110d10bc03fb5239f806e137b7
ChoiHyunSan <chsan626@gmail.com>
2025-01-02 11:14:30 +0900

    스프링 시큐리티 기본 설정 세팅

---
+++ backend/Web-ChattingServer/src/main/java/com/ll/webchattingserver/global/security/UserDetailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.ll.webchattingserver.global.security;
+
+import com.ll.webchattingserver.domain.User;
+import com.ll.webchattingserver.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
