668b225ba0cd9fec6844899ef5dd39ee67c2e568
ChoiHyunSan <chsan626@gmail.com>
2025-01-02 10:51:42 +0900

    백엔드 프로젝트 생성

---
+++ backend/Web-ChattingServer/src/main/java/com/ll/webchattingserver/WebChattingServerApplication.java
@@ -0,0 +1,13 @@
+package com.ll.webchattingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChattingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebChattingServerApplication.class, args);
+    }
+
+}
